151a43da363f009c5ac1a76e0d34632e657213d1
Dan Dee <monkeyjunglejuice@pm.me>
2021-10-06 17:51:47 +0800

    10 recent files are enough in most cases

--- init-onboard.el
+++ init-onboard.el
@@ -702,7 +702,7 @@ or `system-configuration' directly."
 (recentf-mode 1)
 
 (setq recentf-max-menu-items 10
-      recentf-max-saved-items 100)
+      recentf-max-saved-items 10)
 
 
 ;; Ignore some recently visitet files
