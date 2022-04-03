1e982b64c4e07665c44c85071711c76c2417e109
Dan Dee <monkeyjunglejuice@pm.me>
2022-04-03 09:32:51 +0800

    Formatting

--- init-onboard.el
+++ init-onboard.el
@@ -709,10 +709,8 @@ or `system-configuration' directly."
 
 
 ;; Switch search functions to make regex-search the default
-
 (global-set-key (kbd "C-s") #'isearch-forward-regexp)
 (global-set-key (kbd "C-r") #'isearch-backward-regexp)
-
 (global-set-key (kbd "C-M-s") #'isearch-forward)
 (global-set-key (kbd "C-M-r") #'isearch-backward)
 
