a7b6b4a28ceeaa56f49d33ad85087250476eb2da
Dan Dee <monkeyjunglejuice@pm.me>
2022-06-10 15:19:03 +0800

    Fixing a typo

--- init-onboard.el
+++ init-onboard.el
@@ -55,19 +55,19 @@
 
 ;; Tune garbage collection
 
-;; Show a message when garbage collection happens? Useful while tuning the GC
-(setq garbage-collection-messages nil)
-
 ;; Temporarily set a high value of 256 MB to trigger less garbage collections
 ;; during initialization. The Emacs default is a threshold of 800 KB
 (setq gc-cons-threshold (* 256 1000000))
 
-;; Then lower the threshold to 8 MB during normal operation to prevent longer
+;; Then lower the threshold to 4 MB during normal operation to prevent longer
 ;; GC pauses, but still have it at a higher value than the default to
 ;; experience less mini-interruptions – eg. while scrolling larger buffers.
 (add-hook 'emacs-startup-hook
           (lambda ()
             (setq gc-cons-threshold (* 4 1000000))))
+
+;; Show a message when garbage collection happens? Useful while tuning the GC
+(setq garbage-collection-messages nil)
 
 
 ;;; PACKAGE MANAGEMENT ________________________________________________________
