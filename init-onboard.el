992fa95214828888bc3b6509b6b2cbf3f89bebe8
Dan Dee <monkeyjunglejuice@pm.me>
2022-06-12 15:58:58 +0800

    Set gc-cons-threshold to 16 MB

--- init-onboard.el
+++ init-onboard.el
@@ -59,12 +59,12 @@
 ;; during initialization. The Emacs default is a threshold of 800 KB
 (setq gc-cons-threshold (* 256 1000000))
 
-;; Then lower the threshold to 4 MB during normal operation to prevent longer
+;; Then lower the threshold to 16 MB during normal operation to prevent longer
 ;; GC pauses, but still have it at a higher value than the default to
 ;; experience less mini-interruptions – eg. while scrolling larger buffers.
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq gc-cons-threshold (* 4 1000000))))
+            (setq gc-cons-threshold (* 16 1000000))))
 
 ;; Show a message when garbage collection happens? Useful while tuning the GC
 (setq garbage-collection-messages nil)
