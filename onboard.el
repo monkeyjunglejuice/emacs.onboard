7037b66230b5294e427a5ee667d5429b6e0d0cac
Dan Dee <monkeyjunglejuice@pm.me>
2023-03-05 13:05:17 +0800

    Keep using old syntax

--- onboard.el
+++ onboard.el
@@ -1370,7 +1370,7 @@ Kills the current Dired buffer when entering a new directory"
 
 ;; Additional keybinding resembling other sexp-related keybindings
 ;; who usually begin with "C-M". Also useful editing non-lisp languages
-(global-set-key (kbd "<C-M-backspace>") #'backward-kill-sexp)
+(global-set-key (kbd "C-M-<backspace>") #'backward-kill-sexp)
 
 ;;  ____________________________________________________________________________
 (provide 'onboard)
