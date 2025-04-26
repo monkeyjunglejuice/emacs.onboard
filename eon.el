a19bf7f8f9e017d19e438212a1965dc028e59234
Dan Dee <monkeyjunglejuice@pm.me>
2025-04-26 20:06:43 +0800

    v1.4.1

--- eon.el
+++ eon.el
@@ -16,7 +16,7 @@
 ;; Copyright (C) 2021–2025 Dan Dee
 ;; Author: Dan Dee <monkeyjunglejuice@pm.me>
 ;; URL: https://github.com/monkeyjunglejuice/emacs.onboard
-;; Version: 1.4.0
+;; Version: 1.4.1
 ;; Package-Requires: ((EMACS "28.2"))
 ;; Keywords: convenience
 ;; SPDX-License-Identifier: MIT
