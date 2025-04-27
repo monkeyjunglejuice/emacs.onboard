31705b5c16a3d777022ce4eda67ff93043ed7a1c
Dan Dee <monkeyjunglejuice@pm.me>
2025-04-27 18:30:57 +0800

    Formatting

--- eon.el
+++ eon.el
@@ -217,8 +217,10 @@ or `system-configuration' directly."
 
 ;; Make the <Command> key on MacOS act as <Ctrl> key: "C- ..."
 (setq mac-command-modifier 'control)
+
 ;; Make the <Option> key on MacOS act as <Meta> key for "M- ..."
 (setq mac-option-modifier 'meta)
+
 ;; Don't bypass "C-h ..." keybindings
 (setq mac-pass-command-to-system nil)
 
