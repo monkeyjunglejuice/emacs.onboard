8030a64af86a6aefdf233b1401eb476b5ae95d3d
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2026-01-06 18:51:15 +0800

    Change to common subset of GNU/BSD/macOS ls switches for dired.

--- eon.el
+++ eon.el
@@ -2342,8 +2342,7 @@ pretending to clear it."
    ;; Don't accumulate useless Dired buffers
    dired-kill-when-opening-new-dired-buffer t
    ;; Listing columns; Switch arguments with "C-u s" e.g. hide backups with -B
-   dired-listing-switches
-   "-l --almost-all --classify=always --human-readable --group-directories-first"
+   dired-listing-switches "-l -A -h -F"
    ;; Copy files/directories with sub-directories?
    dired-recursive-copies 'always
    ;; Create directories if they don't exist?
