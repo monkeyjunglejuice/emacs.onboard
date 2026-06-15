717629b79824c1aafaf789db88eb50b07ad38994
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2026-06-15 19:38:17 +0800

    v2.6.16

--- eon.el
+++ eon.el
@@ -10,7 +10,7 @@
 ;;    ▒░▒░▒░  ▒░      ▒░ ▒░▒░▒░▒░     ▒░▒░▒░  ▒░      ▒░ ▒░      ▒░ ▒░▒░▒░▒░
 ;;
 ;;
-;; Version: 2.6.15
+;; Version: 2.6.16
 ;; URL: https://github.com/monkeyjunglejuice/emacs.onboard
 ;; Package: eon
 ;; Package-Requires: ((emacs "30.1"))
